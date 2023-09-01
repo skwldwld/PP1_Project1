607659a4a6dd2275cb5d53272b73c0e44aee4d12
skwldwld <yezi0512@handong.ac.kr>
2023-09-01 16:51:12 +0900

    create project

---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,35 @@
+package org.example;
+
+public class Student {
+    private String hno;
+    private String name;
+
+    public Student() {}
+
+    public String getHno() {
+        return hno;
+    }
+
+    public Student(String hno, String name) {
+        this.hno = hno;
+        this.name = name;
+    }
+
+    public void setHno(String hno) {
+        this.hno = hno;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student : " + hno + " - " + name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
