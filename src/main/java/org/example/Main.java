454addbd1059453b72fb3dfc139b0403accdb960
skwldwld <yezi0512@handong.ac.kr>
2023-09-01 16:55:56 +0900

    create project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         Student s1 = new Student("219000", "홍길동");
-        System.out.print(s1.toString());
+        System.out.print(s1.toString());//
     }
 }
